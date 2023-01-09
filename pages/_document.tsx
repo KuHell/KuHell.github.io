11dab1b4368c1377567e85c9eacfb7e7137e92dd
KuHell <itkdgus489@gmail.com>
2023-01-09 22:26:08 +0900

    rm: body

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      {/* <body className="scrollbar-hide"> */}
       <body className="scrollbar-hide">
         <Main />
         <NextScript />
